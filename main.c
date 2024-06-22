d5f272ef808712be5871c473266ee29d7c518596
Fernando Castro <129780150+wolferfoxx@users.noreply.github.com>
2024-06-21 21:51:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -12,43 +12,82 @@ int main() {
     float compression_rate_8bits, compression_rate_min_bits;
 
     while (1) {
+        printf("-> ");
         if (scanf("%s", command) != 1) {
             fprintf(stderr, "Erro ao ler o comando\n");
             continue;
         }
 
+        int cmd = 0;
         if (strcmp(command, "add") == 0) {
-            if (scanf(" %c %d", &character, &frequency) != 2) {
-                fprintf(stderr, "Erro ao ler o caractere e a frequência\n");
-                continue;
-            }
-                add(character, frequency);
+            cmd = 1;
         } else if (strcmp(command, "calc") == 0) {
-                calc();
+            cmd = 2;
         } else if (strcmp(command, "print") == 0) {
-                print();
+            cmd = 3;
         } else if (strcmp(command, "cod") == 0) {
-            if (scanf("%s", str) != 1) {
-                fprintf(stderr, "Erro ao ler a string\n");
-                continue;
-            }
-            char* encoded_str = cod(str, &compression_rate_8bits, &compression_rate_min_bits);
-            printf("%s %.2f%% %.2f%%\n", encoded_str, compression_rate_8bits, compression_rate_min_bits);
-            free(encoded_str);
+            cmd = 4;
         } else if (strcmp(command, "dec") == 0) {
-            if (scanf("%s", bits) != 1) {
-                fprintf(stderr, "Erro ao ler os bits\n");
-                continue;
-            }
-            char* decoded_str = dec(bits);
-            printf("%s\n", decoded_str);
-            free(decoded_str);
+            cmd = 5;
         } else if (strcmp(command, "clear") == 0) {
-                clear();
+            cmd = 6;
         } else if (strcmp(command, "exit") == 0) {
-            break;
+            cmd = 7;
+        }
+
+        switch (cmd) {
+            case 1:
+                if (scanf(" %c %d", &character, &frequency) != 2) {
+                    fprintf(stderr, "Erro ao ler o caractere e a frequência\n");
+                    continue;
+                }
+                add(character, frequency);
+                break;
+            
+            case 2:
+                calc();
+                break;
+
+            case 3:
+                print();
+                break;
+
+            case 4:
+                if (scanf("%s", str) != 1) {
+                    fprintf(stderr, "Erro ao ler a string\n");
+                    continue;
+                }
+                {
+                    char* encoded_str = cod(str, &compression_rate_8bits, &compression_rate_min_bits);
+                    printf("%s %.2f%% %.2f%%\n", encoded_str, compression_rate_8bits, compression_rate_min_bits);
+                    free(encoded_str);
+                }
+                break;
+
+            case 5:
+                if (scanf("%s", bits) != 1) {
+                    fprintf(stderr, "Erro ao ler os bits\n");
+                    continue;
+                }
+                {
+                    char* decoded_str = dec(bits);
+                    printf("%s\n", decoded_str);
+                    free(decoded_str);
+                }
+                break;
+
+            case 6:
+                clear();
+                break;
+
+            case 7:
+                return 0;
+            
+            default:
+                fprintf(stderr, "Comando inválido\n");
+                break;
         }
     }
 
-    return 0;
+    return 0;
 }
